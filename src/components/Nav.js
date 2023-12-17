2599c5522d6b5a4ab6191cb38efd1e60f4292425
Apurv Prajapati <apurv.prajapati27@gmail.com>
2023-12-17 18:22:52 +0530

    commit :: some changes Nav.js file

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,17 +1,34 @@
-import React from "react";
-import littlelemon_logo from "../images/littlelemon_logo.png";
+import React, { useState } from "react";
+import logo from "../images/littlelemon_logo.png";
 import { Link } from "react-router-dom";
 
 const Nav = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
   return (
-    <nav>
-      <img src={littlelemon_logo} alt="Little Lemon Logo"></img>
-      <ul>
+    <nav className={`navbar ${menuOpen ? "open" : ""} `}>
+      <a href="/" className="logo">
+        <img src={logo} alt="logo" />
+      </a>
+      {/*moblie navbar*/}
+      <div className="menu-icon" onClick={toggleMenu}>
+        <div className="bar"></div>
+        <div className="bar"></div>
+        <div className="bar"></div>
+      </div>
+      {/*nav items*/}
+      <ul className={`nav-links ${menuOpen ? "visible" : ""} `}>
         <li>
           <Link to={"/"}>Home</Link>
         </li>
         <li>
           <Link to={"/"}>About</Link>
+        </li>
+        <li>
+          <Link to={"/"}>Services</Link>
         </li>
         <li>
           <Link to={"/"}>Menu</Link>
