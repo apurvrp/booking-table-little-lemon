0cae49d7e2212139274d2b7a793377903880361c
Apurv Prajapati <apurv.prajapati27@gmail.com>
2023-12-17 19:11:57 +0530

    commit  :: some changes in files

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Nav from "./components/Nav";
 import Main from "./components/Main";
 import Footer from "./components/Footer";
 import "./App.css";
+import Menu from "./components/Menu";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Fragment>
         <Nav />
         <Main />
+        <Menu />
         <Footer />
       </Fragment>
     </div>
